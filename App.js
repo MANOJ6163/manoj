4ab4e8ac73eaeaa2e5cfb2550063ef410d81ff77
MANOJ6163 <manoj6163@gmail.com>
2025-05-02 12:57:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Your IT Company</h1>
+        <p>Exploring the future of IT with dynamic pages and interactive content.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
